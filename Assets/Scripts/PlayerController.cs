8ec50d1c79dcdb7bedbd2171b48773f6a4f7380e
jennslaptop <111903622+jen0502@users.noreply.github.com>
2022-09-08 20:25:55 +0000

    Sep 8, 2022, 4:25 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+
     private Rigidbody rb;
+
     private float movementX;
     private float movementY;
 
@@ -10,7 +18,7 @@
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -18,9 +26,11 @@
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
     }
+
+}
